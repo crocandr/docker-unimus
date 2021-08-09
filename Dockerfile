7afc394b87d377e1748511d53970b96c7db1d3fb
crocandr <slezsak.andras@gmail.com>
2021-08-09 19:20:30 +0200

    jre changed to jdk for jarsigner command

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ ENV DOWNLOAD_URL https://unimus.net/download-unimus/dev-builds/Unimus.jar
 RUN apt-get update && apt-get install -y curl vim less wget tzdata
 
 # OpenJDK
-RUN apt-get install -y openjdk-11-jre-headless
+RUN apt-get install -y openjdk-11-jdk-headless
 #
 # Unimus 
 RUN curl -L -o /opt/unimus.jar $DOWNLOAD_URL
--- files/start.sh
+++ files/start.sh
@@ -9,6 +9,11 @@ JAVA_EXTRA_PARAMS=""
 [ -z "$TZ" ] && { TZ="UTC"; }
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
+# verify jar
+if [ $( which jarsigner | wc -l ) -gt 0 ]
+then
+  jarsigner -verify /opt/unimus-core.jar | grep -i "jar verified" || { echo "Unimus binary is not verified"; exit 1; }
+fi
 
 # run
 java $JAVA_EXTRA_PARAMS -jar /opt/unimus.jar
